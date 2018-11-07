85da227cfa146ff2b0e5292bce90f41972d4101d
zero <1215850394@qq.com>
2018-11-08 00:34:41 +0800

    step -a

--- template.py
+++ template.py
@@ -1,18 +1,40 @@
 from flask import Flask, render_template, request
-
+from flask_bootstrap  import Bootstrap
 
 
 app = Flask(__name__)
 
 
+bootstrap = Bootstrap(app)
+
 @app.route('/')
 def index():
 
-	return render_template('index.html', request = request)
+	comments = [
+		{
+			'username' : 'zero',
+			'comment' : 'hello flask '
+		},
+		{
+			'username': 'dancesmile',
+			'comment': ' falsk template '
+		},
+		{
+			'username': 'butterfly',
+			'comment': ' the free'
+		}
+	]
+
+	return render_template('index.html', request = request, comments = comments)
+
+@app.route('/user')
+def user():
+	return render_template('user.html')
 
 
-
-
+@app.errorhandler(404)
+def page_404(e):
+	return render_template('404.html')
 if __name__ == '__main__': 
 
 	app.run(debug=True)
